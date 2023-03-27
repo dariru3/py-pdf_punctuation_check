9de103c0a1bb08a90e2cf3d4c48394be9f3df9e5
Daryl V <daryl.villalobos@hotmail.com>
2023-03-27 10:35:12 +0900

    Try EA unicode re

--- test_fw.py
+++ test_fw.py
@@ -24,8 +24,26 @@ def check_full_width2(text):
     print("Check 2:", temp_set)
     return temp_set
 
-text = "™㎎㎏㎚㎛㎜㎝㎞㎟㎠㎢㎣㎤㎦㎽㎾㎿ℓ№™₤…ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩⅪⅫⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹⅺⅻ△"
-print("text len:", len(text))
+def check_full_width3(text):
+    pattern = re.compile(r"[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\uFF01-\uFF5E\uFF65-\uFF9F]+")
+    temp_set = set()
+    for char in text:
+        if pattern.search(char) is not None:
+            temp_set.add(char)
+    if temp_set == set():
+        temp_set = "empty"
+    print("Check 3:", temp_set)
+    return temp_set
 
+text = "™㎎㎏㎚㎛㎜㎝㎞㎟㎠㎢㎣㎤㎦㎽㎾㎿ℓ№™₤…ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩⅪⅫⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹⅺⅻ△"
+# print("text len:", len(text))
+print("test 1")
 check_full_width(text)
 check_full_width2(text)
+check_full_width3(text)
+
+text2 = '【】「」・※●（）％＆℃㎡㎥：￥'
+print("test 2")
+check_full_width(text2)
+check_full_width2(text2)
+check_full_width3(text2)
