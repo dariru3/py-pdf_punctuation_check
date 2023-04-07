0b6921a9570c183b09d129d2e7ff5157c064d350
Daryl V <daryl.villalobos@hotmail.com>
2023-04-07 16:46:56 +0900

    Unit test with extracted text

--- test_check_fw.py
+++ test_check_fw.py
@@ -31,19 +31,13 @@ def check_full_width(text):
 
 class TestCheckFullWidth(unittest.TestCase):
     def test_check_full_width(self):
-        test_strings = [string1, string2, string3]
-        for string in test_strings:
-            with self.subTest(string):
-                result_set = check_full_width(string)
-                for char in result_set:
-                    self.assertIn(char, full_width_chars, f'Expected {char} to be in {full_width_chars}')
+        file_path = "output_file.txt"
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            
+        result_set = check_full_width(file_content)
+        for char in result_set:
+            self.assertIn(char, full_width_chars, f'Expected {char} to be in {full_width_chars}')
 
-# if __name__ == '__main__':
-#     unittest.main()
-
-file_path = "output_file.txt"
-with open(file_path, 'r', encoding='utf-8') as file:
-    file_content = file.read()
-
-results = check_full_width(file_content)
-print(results)
+if __name__ == '__main__':
+    unittest.main()
