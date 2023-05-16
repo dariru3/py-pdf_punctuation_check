3ad0ac9ca58e34dc6c2259e09b529d5cb833b259
Daryl V <daryl.villalobos@hotmail.com>
2023-05-16 13:36:39 +0900

    Fix repeated punct regex

--- test_punctuation.py
+++ test_punctuation.py
@@ -1,11 +1,29 @@
-import unittest
 from main import check_punctuation_patterns
 
-def testParethesis():
-    test_sentence = "This is missing a (closing parenthesis)."
+def testPunctuation():
+    test_sentence = "Hello??!!??"
     test_summary = []
     errors = check_punctuation_patterns(test_sentence, test_summary)
     print(errors)
+    print(f"errors: {test_summary}")
 
 if __name__ == '__main__':
-    testParethesis()
+    testPunctuation()
+
+import re
+
+def has_repeated_punctuation(text):
+    pattern = re.compile(r"([.,;:?!'\[\]{}()“”‘’&%$¥—-])\1")
+    match = pattern.search(text)
+    return match is not None
+
+def test_repeated_punctuation():
+    test_sentence = "hello??"
+    if has_repeated_punctuation(test_sentence):
+        print("The sentence has repeated punctuation.")
+    else:
+        print("The sentence does not have repeated punctuation.")
+
+if __name__ == '__main__':
+    pass
+    #test_repeated_punctuation()
