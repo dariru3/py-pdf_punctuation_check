18fa3c89e048c7c72d9e7b400c2cac8814d8c97d
Daryl V <daryl.villalobos@hotmail.com>
2023-05-22 10:50:26 +0900

    Clean up simple test

--- test_punctuation.py
+++ test_punctuation.py
@@ -1,29 +1,15 @@
 from main import check_punctuation_patterns
 
 def testPunctuation():
-    test_sentence = "(Hello World! (Is it not great?"
+    test_sentence = """
+    Hello World!!
+    It's me . Hi!
+    Are you not entertained??
+    """
     test_summary = []
     errors = check_punctuation_patterns(test_sentence, test_summary)
-    print(errors)
+    # print(errors)
     print(f"errors: {test_summary}")
 
 if __name__ == '__main__':
     testPunctuation()
-
-import re
-
-def has_repeated_punctuation(text):
-    pattern = re.compile(r"([.,;:?!'\[\]{}()“”‘’&%$¥—-])\1")
-    match = pattern.search(text)
-    return match is not None
-
-def test_repeated_punctuation():
-    test_sentence = "hello??"
-    if has_repeated_punctuation(test_sentence):
-        print("The sentence has repeated punctuation.")
-    else:
-        print("The sentence does not have repeated punctuation.")
-
-if __name__ == '__main__':
-    pass
-    #test_repeated_punctuation()
